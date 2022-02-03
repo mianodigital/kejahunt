263affa8bfd380271e9df548097554109c765988
Miano Digital <mianodigital@gmail.com>
2022-02-03 20:04:38 +0300

    Update Content Folder
    Added sanity, created new schemas, property, user, comment, postedBy

--- content/schemas/schema.js
+++ content/schemas/schema.js
@@ -16,7 +16,7 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    /*Enter Your types here! */
     comment,
     property,
     postedBy,
